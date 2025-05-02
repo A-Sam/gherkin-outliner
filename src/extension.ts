a60f82fe3c641e42764cadcb9c7469c920e5e9cb
Sam <eng.a7med.sam@gmail.com>
2025-05-02 13:06:48 +0200

    Update extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -20,8 +20,6 @@ function applyDecorations(editor: vscode.TextEditor) {
 }
 
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	const provider = new BDDOutlineProvider();
 	const selector = { language: 'cpp', scheme: 'file' };
@@ -49,10 +47,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('gherkin-outliner.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from gherkin-outliner!');
+	const disposable = vscode.commands.registerCommand('gherkin-outliner', () => {
 	});
 
 	context.subscriptions.push(disposable);
